e7550032b21efb3c40c861820368c28037f4cddd
Sheikh-77 <nazreenshaikh0077@gmail.com>
2021-09-17 23:00:20 +0530

    custom styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'custom-green': '#295A5F',
+      },
+      borderWidth: {
+        10: '10px',
+      },
+    },
   },
   variants: {
     extend: {},
