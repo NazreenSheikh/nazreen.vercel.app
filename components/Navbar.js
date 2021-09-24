8ee8a887633f5886c0376cf4aebb5a7f92dfa89d
Sheikh-77 <nazreenshaikh0077@gmail.com>
2021-09-24 12:33:06 +0530

    done styling

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,7 +1,7 @@
 export default function Skills() {
   return (
     <nav className="flex justify-between sticky-nav px-1 py-2 lg:h-20 lg:px-12 lg:items-center md:items-center md:h-14  md:px-8 ">
-      <h2 className="text-custom-green tracking-wider font-bold lg:text-4xl lg:tracking-widest md:text-3xl">
+      <h2 className="text-custom-green tracking-wider font-bold lg:text-5xl lg:tracking-widest md:text-3xl">
         NAZREEN
       </h2>
       <ul className="flex justify-between space-x-2 text-gray-600 text-sm lg:text-2xl lg:space-x-12 md:text-lg md:space-x-4">
