55cfb33a2daa82cf2c1bc2bf01ed24dfffa6743a
Sheikh-77 <nazreenshaikh0077@gmail.com>
2021-09-28 23:54:43 +0530

    Home file

--- pages/index.js
+++ pages/index.js
@@ -9,23 +9,23 @@ export default function Home() {
   return (
     <div
       id="Home"
-      className="box-border p-2 md:h-screen text-custom-green md:flex "
+      className="box-border md:h-screen text-custom-green md:flex "
     >
-      <div className="md:w-8 md:h-4/5 md:my-auto md:ml-4 bg-custom-green "></div>
-      <div className="md:mt-28 md:mb-auto md:ml-12 ">
+      <div className="lg:w-8 md:w-7 md:h-4/5 md:my-auto md:ml-4 bg-custom-green "></div>
+      <div className="px-4 mt-4 md:m-auto lg:ml-12 md:ml-4">
         <div>
-          <p className="text-2xl text-black lg:text-5xl md:text-3xl ">
+          <p className="text-2xl text-black lg:text-5xl md:text-4xl ">
             HI, I am Nazreen
           </p>
-          <p className="items-center inline-block text-4xl font-extrabold md:mt-8 lg:text-7xl md:text-5xl ">
+          <p className="items-center mt-2 text-4xl font-extrabold md:mt-8 lg:text-6xl md:text-5xl ">
             Full Stack Web Developer And Designer
           </p>
         </div>
-        <div className="text-xl text-center text-custom-green md:mt-4 md:text-2xl md:text-left md:-bottom-32 ">
-          <button className="p-2 m-4 text-black bg-custom-green md:p-4">
+        <div className="text-xl text-center text-custom-green md:mt-4 md:text-xl md:text-left md:-bottom-32 ">
+          <button className="p-3 mx-4 mt-6 text-white bg-custom-green md:p-4 ">
             <a href="#Contact">CONTACT ME</a>
           </button>
-          <ul className="flex justify-between w-3/5 p-2 m-auto text-3xl text-center text-black md:mt-4 md:text-5xl md:w-1/2 md:my-auto md:mx-0 ">
+          <ul className="flex justify-between w-3/5 px-4 mx-auto mt-8 text-4xl text-center text-black md:mt-6 md:text-5xl md:w-1/2 md:mx-0 ">
             <li>
               <a href="http://www.twitter.com/nazreenshaikh6">
                 <AiFillTwitterCircle />
@@ -49,12 +49,12 @@ export default function Home() {
           </ul>
         </div>
       </div>
-      <div className="p-4 m-auto text-center md:w-11/12 lg:w-11/12 md:p-0 ">
+      <div className="p-4 m-auto text-center md:w-4/5 lg:w-11/12 md:p-0 ">
         <Image
           src="/Images/Mask Group.svg"
           alt="nazreen's pic"
-          height={500}
-          width={500}
+          height={550}
+          width={550}
         />
       </div>
     </div>
