e7880be6dab60fb2a343f2ee2c5c7c6ea53f1091
Sheikh-77 <nazreenshaikh0077@gmail.com>
2021-09-29 23:53:14 +0530

    remove Extras

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,10 +1,12 @@
 export default function Skills() {
   return (
-    <nav className="flex justify-between px-1 py-2 sticky-nav lg:h-20 lg:px-12 lg:items-center md:items-center md:h-14 md:px-8 ">
+    <nav className="sticky flex justify-between px-1 py-2 h-14 lg:h-20 lg:px-12 md:items-center md:px-8 ">
       <h2 className="font-bold tracking-wider text-custom-green lg:text-5xl lg:tracking-widest md:text-3xl">
-        <a href="#">NAZREEN</a>
+        <a href="#" className="hidden md:block">
+          NAZREEN
+        </a>
       </h2>
-      <ul className="flex justify-between space-x-2 text-sm text-gray-600 lg:text-2xl lg:space-x-12 md:text-lg md:space-x-4">
+      <ul className="flex items-center justify-between mx-auto space-x-12 text-lg text-gray-600 md:mx-0 lg:text-2xl md:space-x-12 md:text-lg md:space-x-4">
         <li>
           <a href="#Home">Home</a>
         </li>
