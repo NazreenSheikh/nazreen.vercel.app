7cb6b90ed7b84cc4765cc799aacaec6f6abc271c
Sheikh-77 <nazreenshaikh0077@gmail.com>
2021-10-05 21:47:09 +0530

    added Footer file

---
+++ components/Footer.js
@@ -0,0 +1,9 @@
+export default function Footer() {
+  return (
+    <footer className="bg-custom-green">
+      <p className="p-10 mt-8 text-center text-gray-300 md:mt-0 md:text-lg">
+        &copy;2021 Nazreen All rights reserved.
+      </p>
+    </footer>
+  )
+}
