996b4513ee67a24f731a746aecb6cb9e9455ab8f
Sheikh-77 <nazreenshaikh0077@gmail.com>
2021-10-12 23:30:22 +0530

    added metatags file

---
+++ components/Metatags.js
@@ -0,0 +1,22 @@
+import Head from 'next/head'
+
+export default function Metatags({
+  title = `Nazreen's protfolio `,
+  description = `I&apos;m a young student passinate about davelopment. I work
+  with decisiveness and conviction. My feild of interest are programming
+  and website designing .`,
+}) {
+  return (
+    <Head>
+      <title>{title}</title>
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:site" content="@NazreenShaikh6" />
+      <meta name="twitter:title" content={title} />
+      <meta name="twitter:description" content={description} />
+
+      {/* <meta name="twitter:image" content={Image} /> */}
+      <meta property="og:title" content={title} />
+      <meta property="og:description" content={description} />
+    </Head>
+  )
+}
