34b265355b3f13049a7be86162f4ff6c70d3b04a
Sheikh-77 <nazreenshaikh0077@gmail.com>
2021-10-17 14:20:33 +0530

    responsive footer

--- components/Footer.js
+++ components/Footer.js
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <footer className="bg-custom-green">
+    <footer className="bg-custom-green container w-full mx-auto max-w-screen-2xl ">
       <p className="p-10 mt-8 text-center text-gray-300 md:mt-0 md:text-lg">
         &copy;2021 Nazreen All rights reserved.
       </p>
