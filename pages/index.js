b76be6d0d627fbef227a67637d7eafc1061557ac
Sheikh-77 <nazreenshaikh0077@gmail.com>
2021-10-25 12:58:38 +0530

    fixed projects

--- pages/index.js
+++ pages/index.js
@@ -29,7 +29,7 @@ export default function Home() {
             <button className="p-3 mx-4 mt-6 text-white bg-custom-green md:p-4 ">
               <a href="#Contact">CONTACT ME</a>
             </button>
-            <ul className="flex justify-between w-3/5 px-4 mx-auto mt-8 text-4xl text-center text-black md:mt-6 md:text-5xl md:w-3/4 md:mx-0 ">
+            <ul className="flex justify-between w-3/5 px-4 mx-auto mt-8  text-center text-black md:mt-6 md:text-5xl md:w-3/4 md:mx-0 ">
               <li>
                 <a href="http://www.twitter.com/nazreenshaikh6">
                   <AiFillTwitterCircle className="hover:text-custom-green" />
