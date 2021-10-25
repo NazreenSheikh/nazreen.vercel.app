5f4cd6dae40cd92c004c6dd0e2cd716244c0f746
Sheikh-77 <nazreenshaikh0077@gmail.com>
2021-10-25 13:26:10 +0530

    typo fixed

--- components/About.js
+++ components/About.js
@@ -11,15 +11,15 @@ export default function About() {
         </div>{' '}
         <div className="bg-white md:w-4 md:h-cus-height md:my-20 md:mr-4 "></div>
         <div className="items-center p-4 mt-12 text-2xl md:w-2/3 item-center md:p-4 md:my-auto lg:text-3xl">
-          Hii, I am Nazreen. Currently Pursuing ELectrical Engeering From JAMIA
-          MILLIA ISLAMIA, DELHI.
-          <br /> I&apos;m a young student passinate about davelopment. I work
-          with decisiveness and conviction. My feild of interest are programming
-          and website designing .
+          Hii, I am Nazreen. Currently Pursuing Electrical Engineering From
+          JAMIA MILLIA ISLAMIA, DELHI.
+          <br /> I'm a young student passionate about development. I work with
+          decisiveness and conviction. My field of interest is programming and
+          website design.
           <br />
           <br /> I have worked on Several Web Development Projects in the past
-          and technologies I prefer Working with include reactjs, nextjs,
-          typescript, python and node js.
+          and the technologies I prefer Working with include reactjs, nextjs,
+          typescript, python, and node js.
         </div>
         <div className="p-8 m-auto text-center md:ml-2 md:p-0">
           <Image
