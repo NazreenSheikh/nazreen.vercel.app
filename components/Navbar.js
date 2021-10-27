55bee444d3908680f6d48e68373eae1d42ad7b26
Sheikh-77 <nazreenshaikh0077@gmail.com>
2021-10-27 14:58:01 +0530

    nav fixed

--- components/Navbar.js
+++ components/Navbar.js
@@ -4,8 +4,8 @@ export default function Skills() {
   return (
     <nav className="sticky flex container w-full mx-auto max-w-screen-2xl justify-between px-1 py-2 h-14 lg:h-20 lg:px-12 md:items-center md:px-8 ">
       <h2 className="font-bold tracking-wider text-custom-green lg:text-5xl lg:tracking-widest md:text-3xl">
-        <Link href="/" className="hidden md:block">
-          <a>NAZREEN</a>
+        <Link href="/">
+          <a className="hidden md:block">NAZREEN</a>
         </Link>
       </h2>
       <ul className="flex items-center justify-between mx-auto space-x-4 text-lg text-gray-600 md:text-xl md:mx-0 lg:text-2xl md:space-x-12 ">
